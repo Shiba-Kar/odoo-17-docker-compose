e304ba7110ed3765414044b9551a0023acac4203
Shiba Prasad Kar <Shibaprasadkar50@gmail.com>
2024-11-16 11:47:56 +0530

    .

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ PORT=$2
 CHAT=$3
 
 # Clone Odoo directory
-git clone --depth=1 https://github.com/Shiba-Ka2/odoo-17-docker-compose $DESTINATION
+git clone --depth=1 https://github.com/Shiba-Kar/odoo-17-docker-compose $DESTINATION
 rm -rf $DESTINATION/.git
 
 # Create PostgreSQL directory
